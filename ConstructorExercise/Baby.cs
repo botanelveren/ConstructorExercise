b93d0a0bb9ed4b8db382ad7f9030520d6a3e2cfd
Botan Elveren <botanelveren@gmail.com>
2025-01-08 15:10:39 +0300

    Add project files.

---
+++ ConstructorExercise/Baby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorExercise
+{
+    public class Baby
+    {
+        private string Name { get; set; }
+        private string LastName { get; set; }
+        private DateTime DateOfBirth { get; set; }
+
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaa!");
+            DateTime DateOfBirth = DateTime.Now;
+        }
+
+        public Baby(string name, string lastName)
+        {
+            Console.WriteLine("Ingaaaa!");
+            Name = name;
+            LastName = lastName;
+            DateTime DateOfBirth = DateTime.Now;
+        }
+        
+        public void Greet()
+        {
+            Console.WriteLine($"Baby: ");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"LastName: {LastName}");
+            Console.WriteLine($"Date of Birth: {DateOfBirth}");
+        }
+
+    }
+}
---
+++ ConstructorExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConstructorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby baby1 = new Baby();
+            baby1.Greet();
+            Baby baby2 = new Baby("Botan", "Elveren");
+            baby2.Greet();
+        }
+    }
+}
